a1299d3e1ae59c22ca939a720f0e916cb691453a
aleks.karczewski <aleks.karczewski@student.put.poznan.pl>
2025-10-07 13:16:52 +0200

    Addied moving through filesystem (showing only directories and .json files)

--- ipScannerServer/Program.cs
+++ ipScannerServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Spectre.Console;
+using System.Text.Json;
 
 namespace MyApp
 {
@@ -6,82 +7,129 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            String currentDir = Directory.GetCurrentDirectory();
 
             int menu = 0;
             var choice = "";
+            int height = AnsiConsole.Console.Profile.Height;
             while (true)
             {
-
+                if (menu == -1)
+                {
+                    break;
+                }
                 if (menu == 0)
                 {
+                    System.Console.Clear();
                     choice = AnsiConsole.Prompt(
                         new SelectionPrompt<string>()
                         .Title("Select an option:")
                         .PageSize(5)
                         .AddChoices(new[] { "Connect to server", "Exit" }));
-                    menu = 1;
-                }
-                else if (menu == 1)
-                {
                     if (choice == "Connect to server")
                     {
-                        var serverConnectionChoice = AnsiConsole.Prompt(
-                        new SelectionPrompt<string>()
-                        .Title("Choose server connection configuration method:")
-                        .PageSize(5)
-                        .AddChoices(new[] { "Load from .JSON", "Input manually", "Return" }));
-
-
-                        if (serverConnectionChoice == "Load from .JSON")
-                        {
-                            System.Console.Clear();
-
-                            var currentDir = Directory.GetCurrentDirectory();
-                            var files = Directory.GetFiles(currentDir);
-                            AnsiConsole.MarkupLine(currentDir);
-
-
-                            if (files.Length > 0)
-                            {
-                                List<string> fileNames = new List<string>();
-
-
-                                foreach (var file in files)
-                                {
-                                    fileNames.Add(Path.GetFileName(file));
-                                }
-
-
-                                var fileSelection = AnsiConsole.Prompt(
-                                new SelectionPrompt<string>()
-                                .Title("Choose file:")
-                                .PageSize(5)
-                                .AddChoices(fileNames));
-
-                                AnsiConsole.MarkupLine($"selected file extension {Path.GetExtension(fileSelection)}");
-                            }
-                            else
-                            {
-                                //what to do here ?
-                            }
-
-
-                        }
-                        else if (serverConnectionChoice == "Input manually")
-                        {
-
-                        }
-                        else if (serverConnectionChoice == "Return")
-                        {
-                            menu = 0;
-
-                        }
-
+                        menu = 1;
                     }
                     else
                     {
-                        break;
+                        menu = -1;
                     }
+                }
+                if (menu == 1)
+                {
+                    System.Console.Clear();
+                    var serverConnectionChoice = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                    .Title("Choose server connection configuration method:")
+                    .PageSize(5)
+                    .AddChoices(new[] { "Load from .JSON", "Input manually", "Return" }));
+
+
+
+
+                    if (serverConnectionChoice == "Load from .JSON")
+                    {
+                        menu = 2;
+                    }
+                    else if (serverConnectionChoice == "Input manually")
+                    {
+                        menu = 3;
+                    }
+                    else if (serverConnectionChoice == "Return")
+                    {
+                        menu = 0;
+
+                    }
+
+
+                }
+                if (menu == 2)
+                {
+                    System.Console.Clear();
+
+
+                    var files = Directory.GetFiles(currentDir);
+                    var directories = Directory.GetDirectories(currentDir);
+                    AnsiConsole.MarkupLine(currentDir);
+
+
+                    if (files.Length > 0 || true)
+                    {
+                        List<string> fileNames = new List<string>();
+
+                        fileNames.Add("..");
+
+                        foreach (var directory in directories)
+                        {
+                            fileNames.Add(Path.GetFileName(directory));
+                        }
+
+                        foreach (var file in files)
+                        {
+                            if (Path.GetExtension(file).ToLower() == ".json")
+                            {
+                                fileNames.Add(Path.GetFileName(file));
+                            }
+
+
+                        }
+
+
+                        var fileSelection = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Choose file:")
+                        .PageSize(height)
+                        .AddChoices(fileNames));
+
+                        if (fileSelection == "..")
+                        {
+                            if (Directory.GetParent(currentDir)?.FullName != null)
+                            {
+                                currentDir = Directory.GetParent(currentDir)?.FullName;
+                            }
+
+
+                        }
+                        else if (fileSelection.ToLower().EndsWith(".json"))
+                        {
+                            Console.WriteLine("COHSEN JSON");
+                            var json = File.ReadAllText(fileSelection);
+                            var obj = JsonSerializer.Deserialize<object>(json);
+                        }
+                        else
+                        {
+                            currentDir += $"\\{fileSelection}";
+                            Console.WriteLine("COHSEN DIRECTORY");
+                        }
+
+                        AnsiConsole.MarkupLine($"selected file extension {Path.GetExtension(fileSelection)}");
+                    }
+                    else
+                    {
+                        //what to do here ?
+                    }
+
+
                 }
             }
         }
