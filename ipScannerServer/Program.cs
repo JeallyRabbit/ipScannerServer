371f21fd928c4252fa466c44b598f54d983f3728
aleks.karczewski <aleks.karczewski@student.put.poznan.pl>
2025-10-20 15:40:59 +0200

    changd names of projects and splitet app into 2 projects (server/client)

--- ipScannerServer/Program.cs
+++ ipScannerServer/Program.cs
@@ -8,22 +8,7 @@ using System.Text.Json.Serialization;
 namespace MyApp
 {
 
-    class Server
-    {
-        [JsonInclude]
-        private string address { get; set; }
-        [JsonInclude]
-        private string port { get; set; }
 
-        public Server(string address, string port)
-        {
-            this.address = address;
-            this.port = port;
-        }
-
-        public string getAddress() => address;
-        public string getPort() => port;
-    }
 
     class Database
     {
@@ -73,7 +58,7 @@ namespace MyApp
     {
         const int MENU_EXIT = -1;
         const int MENU_SERVER_CLIENT = 0;
-        const int MENU_DATABYSE_CONNECTION_TYPE = 1;
+        const int MENU_DATABASE_CONNECTION_TYPE = 1;
         const int MENU_DATABASE_JSON = 2;
         const int MENU_DATABASE_INPUT = 3;
         const int MENU_PROCESS_SERVER_SIDE = 4;
@@ -93,14 +78,13 @@ namespace MyApp
         {
             String currentDir = Directory.GetCurrentDirectory();
 
-            int menu = MENU_SERVER_CLIENT;
+            int menu = MENU_DATABASE_CONNECTION_TYPE;
             var choice = "";
             int height = AnsiConsole.Console.Profile.Height;
             int port = 60719;
             string connectionString = "";
 
 
-            Server serverData = null;
 
             while (true)
             {
@@ -115,21 +99,17 @@ namespace MyApp
                         new SelectionPrompt<string>()
                         .Title("Select an option:")
                         .PageSize(5)
-                        .AddChoices(new[] { "Connect client to server", "Connect server to database", "Exit" }));
+                        .AddChoices(new[] { "Connect server to database", "Exit" }));
                     if (choice == "Connect server to database")
                     {
-                        menu = MENU_DATABYSE_CONNECTION_TYPE;
-                    }
-                    else if (choice == "Connect client to server")
-                    {
-                        menu = MENU_CONNECT_TO_SERVER_TYPE;
+                        menu = MENU_DATABASE_CONNECTION_TYPE;
                     }
                     else
                     {
                         menu = MENU_EXIT;
                     }
                 }
-                if (menu == MENU_DATABYSE_CONNECTION_TYPE)
+                if (menu == MENU_DATABASE_CONNECTION_TYPE)
                 {
                     System.Console.Clear();
                     var serverConnectionChoice = AnsiConsole.Prompt(
@@ -203,7 +183,7 @@ namespace MyApp
                     }
                     else if (fileSelection == SELECTION_BACK)
                     {
-                        menu = MENU_DATABYSE_CONNECTION_TYPE;
+                        menu = MENU_DATABASE_CONNECTION_TYPE;
                     }
                     else
                     {
@@ -364,234 +344,7 @@ namespace MyApp
 
 
                 }
-                else if (menu == MENU_CONNECT_TO_SERVER_TYPE)
-                {
-                    //getting server connection data
-                    System.Console.Clear();
-                    var clientConnectionChoice = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                    .Title("Choose client connection configuration method:")
-                    .PageSize(5)
-                    .AddChoices(new[] { "Load from .JSON", "Input manually", "Return" }));
 
-
-
-
-                    if (clientConnectionChoice == "Load from .JSON")
-                    {
-                        menu = MENU_CLIENT_JSON;
-                    }
-                    else if (clientConnectionChoice == "Input manually")
-                    {
-                        menu = MENU_CLIENT_INPUT;
-                    }
-                    else if (clientConnectionChoice == "Return")
-                    {
-                        menu = MENU_SERVER_CLIENT;
-
-                    }
-                }
-                else if (menu == MENU_CLIENT_JSON)
-                {
-                    System.Console.Clear();
-                    string fileSelection = Program.fileSelection(currentDir, height);
-
-                    if (fileSelection == "..")
-                    {
-                        if (Path.GetDirectoryName(currentDir) != null)
-                        {
-                            currentDir = Path.GetDirectoryName(currentDir);
-                        }
-
-
-                    }
-                    else if (fileSelection.ToLower().EndsWith(".json"))
-                    {
-
-                        Console.WriteLine("Chosen JSON");
-                        var json = File.ReadAllText(fileSelection);
-
-                        try
-                        {
-                            serverData = JsonSerializer.Deserialize<Server>(json);
-                        }
-                        catch (System.Text.Json.JsonException ex)
-                        {
-                            menu = MENU_CLIENT_JSON;
-                            AnsiConsole.MarkupLine($"[red]Failed to read:[/] {fileSelection}");
-                            AnsiConsole.MarkupLine($"[grey]{ex.Message}[/]");
-                        }
-
-                        if (serverData != null)
-                        {
-                            // create required objects to connect to server
-                            //add pringint what was read
-                            var jsonPrint = new Spectre.Console.Json.JsonText(
-                           $$"""
-                            { 
-                                "serverAddress": "{{serverData.getAddress()}}", 
-                                "portNumber": "{{serverData.getPort()}}"
-                            }
-                        """);
-
-                            AnsiConsole.Write(
-                                new Panel(jsonPrint)
-                                    .Header("Data read from JSON")
-                                    .Collapse()
-                                    .RoundedBorder()
-                                    .BorderColor(Color.Yellow));
-
-                            if (serverData.getAddress() is null || serverData.getPort() is null)
-                            {
-                                AnsiConsole.MarkupLine("[red]Failed to read data[/]");
-
-                                AnsiConsole.Markup("[grey]Press [bold]<Enter>[/] to continue...[/]");
-                                Console.ReadLine();
-                                menu = MENU_CLIENT_JSON;
-                            }
-                            else
-                            {
-                                menu = MENU_PROCESS_CLIENT;
-                                AnsiConsole.Markup("[grey]Press [bold]<Enter>[/] to continue...[/]");
-                                Console.ReadLine();
-                            }
-
-
-
-                        }
-                        else
-                        {
-                            AnsiConsole.MarkupLine($"[yellow]File loaded but empty/null object:[/] {fileSelection}");
-                        }
-
-
-
-                    }
-                    else if (fileSelection == SELECTION_BACK)
-                    {
-                        menu = MENU_CONNECT_TO_SERVER_TYPE;
-                    }
-                    else
-                    {
-                        currentDir += $"\\{fileSelection}";
-                    }
-
-                    Console.ReadLine();
-
-                }
-                else if (menu == MENU_CLIENT_INPUT)
-                {
-                    var serverNameInput = AnsiConsole.Prompt(
-                    new TextPrompt<string>("[[localhost]] Enter server hostname/ipAddress?:").AllowEmpty());
-                    var serverName = (serverNameInput != "") ? serverNameInput : "localhost";
-
-                    var portInput = AnsiConsole.Prompt(
-                    new TextPrompt<string>("[[60719]] Enter port number:").AllowEmpty());
-                    string portNumber = (portInput != "") ? portInput : "60719";
-
-
-
-                    var ifSave = AnsiConsole.Prompt(
-                    new TextPrompt<bool>("Save to .json?")
-                        .AddChoice(true)
-                        .AddChoice(false)
-                        .DefaultValue(true)
-                        .WithConverter(choice => choice ? "y" : "n"));
-
-
-                    var jsonPrint = new Spectre.Console.Json.JsonText(
-                           $$"""
-                            { 
-                                "serverName": "{{serverName}}", 
-                                "portNumber": "{{portNumber}}"
-                            }
-                        """);
-
-                    AnsiConsole.Write(
-                        new Panel(jsonPrint)
-                            .Header("Data saved to JSON")
-                            .Collapse()
-                            .RoundedBorder()
-                            .BorderColor(Color.Yellow));
-
-
-                    if (ifSave == true)
-                    {
-                        Server toSave = new Server(serverName, portNumber);
-
-                        string serializedToSave = JsonSerializer.Serialize(toSave);
-
-                        string fileName = currentDir + Path.DirectorySeparatorChar + "clientConnection_" + toSave.getAddress() + ".json";
-                        bool isSavingSucces = true;
-                        try
-                        {
-                            File.WriteAllText(fileName, serializedToSave);
-                        }
-                        catch (System.UnauthorizedAccessException)
-                        {
-                            isSavingSucces = false;
-                            AnsiConsole.MarkupLine($"[red]Acces denied[/] to: {fileName}");
-                        }
-
-                        if (isSavingSucces)
-                        {
-                            AnsiConsole.MarkupLine($"[green]Saved to:[/] {fileName}");
-                        }
-
-
-                        AnsiConsole.Markup("[grey]Press [bold]<Enter>[/] to continue...[/]");
-                        Console.ReadLine();
-
-                    }
-                    menu = MENU_PROCESS_CLIENT;
-
-
-                }
-                else if (menu == MENU_PROCESS_CLIENT)
-                {
-
-                    System.Console.Clear();
-                    AnsiConsole.MarkupLine("Processing client side");
-
-
-                    url = $"https://{serverData.getAddress()}/{serverData.getPort()}/api/pcs/oldest";
-
-
-                    using var client = new HttpClient();
-                    client.Timeout = TimeSpan.FromSeconds(5);
-
-                    try
-                    {
-                        List<IP> addresses = client.GetFromJsonAsync<List<IP>>(url).GetAwaiter().GetResult();
-
-                        if (addresses is null)
-                        {
-                            AnsiConsole.MarkupLine($"No data acquired from server");
-                            //Add sleeping here ?
-                        }
-                        else
-                        {
-                            foreach (var address in addresses)
-                            {
-                                AnsiConsole.MarkupLine($"{address.address}, {address.hostname}, {address.lastCheckedDate}");
-
-                                AnsiConsole.Markup("[grey]Press [bold]<Enter>[/] to continue...[/]");
-                                Console.ReadLine();
-
-                            }
-                        }
-
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"[Error] {ex.Message}");
-                        AnsiConsole.Markup("[grey]Press [bold]<Enter>[/] to continue...[/]");
-                        Console.ReadLine();
-                    }
-
-
-                }
             }
         }
 
